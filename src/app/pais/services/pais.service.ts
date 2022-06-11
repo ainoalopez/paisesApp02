4e07cd8e62c851737a6312c07005be790ca71ccf
ainoalopez <ainoalopez@iti.es>
2022-06-11 09:55:01 +0200

    optimixación de peticiones http

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable, of, tap } from 'rxjs';
 import { Country } from '../interfaces/pais.interface';
 // import { catchError } from 'rxjs/operators';
 
@@ -9,15 +9,18 @@ import { Country } from '../interfaces/pais.interface';
 })
 export class PaisService {
 
-  private apiUrl: string = 'https://restcountries.com/v3.1';
+  private apiUrl        : string = 'https://restcountries.com/v3.1';
+  private apiUrlRegiones: string = 'https://restcountries.com/v2';
 
-  // https://restcountries.com/v2/regionalbloc/{regionalbloc}
+  get httpParams () {
+    return new HttpParams().set( 'fields', 'name,capital,cca2,flags,population' );
+  }
 
   constructor( private http: HttpClient ) { }
 
   buscarPais( termino: string ): Observable<Country[]> {
     const url = `${ this.apiUrl }/name/${ termino }`;
-    return this.http.get<Country[]>( url ); // regresa un observable de tipo Country(interface)
+    return this.http.get<Country[]>( url, { params: this.httpParams } ); // regresa un observable de tipo Country(interface)
     // .pipe(
     //   catchError( err => of([])) // el of es una función que genera observables y transforma los que haya dentro de los paréntesis en el observable
     //   );
@@ -25,7 +28,7 @@ export class PaisService {
 
   buscarCapital( termino: string ): Observable<Country[]> {
     const url = `${ this.apiUrl }/capital/${ termino }`;
-    return this.http.get<Country[]>( url );
+    return this.http.get<Country[]>( url, { params: this.httpParams } );
   }
 
   getPaisPorAlpha( id: string ): Observable<Country> {
@@ -34,8 +37,11 @@ export class PaisService {
   }
 
   buscarRegion( region: string ): Observable<Country[]> {
-    const url = `https://restcountries.com/v2/regionalbloc/${ region }`;
-    return this.http.get<Country[]>( url );
+
+    const url = `${this.apiUrlRegiones}/regionalbloc/${ region }`;
+
+    return this.http.get<Country[]>( url, { params: this.httpParams } ).pipe( tap( console.log ) )
+
   }
   
 }
